ec6334571056bc6a3c6b5d9ea2212e74f961fa0d
cmhnk <corinne@automatedinsights.com>
2017-07-10 20:03:20 -0400

    Add scss variable for color; add hover animation; add jquery to log when you click on root

--- app/src/main.js
+++ app/src/main.js
@@ -2,4 +2,8 @@ import './styles/base.scss';
 
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    $('#root').click(function(){
+      console.log( "yay you clicked it!" );
+    });
 });
