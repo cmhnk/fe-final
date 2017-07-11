61b2f2eaf408e8c59801b3a92e5fe89012c2542b
cmhnk <corinne@automatedinsights.com>
2017-07-10 23:12:28 -0400

    Add linkedin and instagram icons; add media queries for additional icons

--- app/src/main.js
+++ app/src/main.js
@@ -3,21 +3,19 @@ import './styles/base.scss';
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('#octocat').click(function(){
-      console.log( "yay you clicked it!" );
-
+    $('.icon-1').click(function(){
       window.location.href='https://github.com/cmhnk';
       return false;
-
     });
 
-    $('#root').click(function(){
-      console.log( "yay you clicked it!" );
-
-      window.location.href='https://github.com/cmhnk';
+    $('.icon-2').click(function(){
+      window.location.href='https://www.linkedin.com/in/corinne-henk-774459108/';
       return false;
-
     });
 
+    $('.icon-3').click(function(){
+      window.location.href='https://www.instagram.com/corinnehenk/?hl=en';
+      return false;
+    });
 
 });
